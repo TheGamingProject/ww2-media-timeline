423ea86d2202b1eed35ea238a294b3e09e6afbb5
Alton Chaney <alton.chaney@gmail.com>
2014-11-01 17:50:06 -0500

    fix js links; add bottom gradient; add extra popover/tooltip helpers

--- app/js/placementUtils.js
+++ app/js/placementUtils.js
@@ -1,1 +1,11 @@
-//$(function() {$('.timeline').css({'margin-top' : -$(this).height()/2});});
+$(function() {
+	$('.column-scroll-content.top').css({
+		'margin-top' : $('.column-scroll-container').height()-$('.column-scroll-content').innerHeight()
+		});
+	});
+	
+	
+$(document).ready(function() {
+	$('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
+    $('[data-toggle="popover"]').popover({html: true, trigger: 'click','placement': 'right'});
+});
