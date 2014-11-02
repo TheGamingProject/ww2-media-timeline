a111c0e0e0f596ada94b92057ef2f7b73b87fc5c
Alton Chaney <alton.chaney@gmail.com>
2014-11-02 17:19:44 -0600

    fix modal backdrop; popover styling (still have width issue)

--- app/js/placementUtils.js
+++ app/js/placementUtils.js
@@ -1,8 +1,12 @@
 $(function() {
 	$('.column-scroll-content.top').css({
 		'margin-top' : $('.column-scroll-container').height()-$('.column-scroll-content').innerHeight()
-		});
 	});
+		
+	$('.popover').css({
+		'width' : $('.popover-content').innerWidth()
+	});
+});
 	
 	
 $(document).ready(function() {
