68ce42859e128c07da11118cdcbaf5eb1fab9a11
Alton Chaney <alton.chaney@gmail.com>
2014-11-02 18:03:16 -0600

    add initial toggle function

--- app/js/placementUtils.js
+++ app/js/placementUtils.js
@@ -13,3 +13,7 @@ $(document).ready(function() {
 	$('[data-toggle="tooltip"]').tooltip({'placement': 'top'});
     $('[data-toggle="popover"]').popover({html: true, trigger: 'click','placement': 'right'});
 });
+
+$('#toggle-info').click(function() {
+    $('.infopanel').animate({left: 0})
+});
